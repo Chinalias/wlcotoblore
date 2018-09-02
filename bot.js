08440b4064b59dd43d5b88c3f860b231c2ea2d93
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-09-02 02:44:19 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+ 
+});
+ 
+client.on('guildMemberAdd' ,member => {
+  if(member.guild.id !== '485579255091298306') return;
+  setTimeout(function(){
+  member.guild.channels.find(r => r.id === '451806679747788800').send('Welcome To **Bُubbles** Tumblr .');
+},3000);
+});
+var PrEfix = "$";
+client.on('message', message => {
+  if (!message.content.startsWith(PrEfix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '485579255091298306') return;
+if (message.content.startsWith(PrEfix + 'wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(PrEfix + 'ls')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(PrEfix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Baktash_183");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
+
+client.login(process.env.BOT_TOKEN);
